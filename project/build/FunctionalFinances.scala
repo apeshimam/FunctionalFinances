78ee3e6a1e144bf03182743fdef06c3c51d986f5
Artie Pesh-Imam <apeshimam@gmail.com>
2011-03-31 17:48:55 -0700

    Removing mistakenly checked in files

--- project/build/FunctionalFinances.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-
-class HelloWorldProject(info: ProjectInfo) extends DefaultProject(info) 
-{
-  val scalatools_release = "Scala Tools Releases" at "http://scala-tools.org/repo-releases/"
-
-  val scalatest = "org.scalatest" % "scalatest" % "1.3" % "test"
-  
-}
